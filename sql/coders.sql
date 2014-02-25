a5dfe37519a842d0d80a105f3dcf6881c30b81a5
Joel Gullander <Joel.gullander@hotmail.com>
2014-02-25 14:01:42 +0100

    Databas edit

--- sql/coders.sql
+++ sql/coders.sql
@@ -7,29 +7,28 @@ CREATE DATABASE coderspool;
 USE coderspool;
 
 CREATE TABLE accounts (
-  id            INT AUTO_INCREMENT PRIMARY KEY,
-  username      VARCHAR(32),
-  password      VARCHAR(32)
+  username      VARCHAR(32) NOT NULL PRIMARY KEY,
+  password      VARCHAR(32) NOT NULL,
 );
 
 CREATE TABLE appliers (
   id            INT AUTO_INCREMENT PRIMARY KEY,
-  account_id    INT,
+  username	VARCHAR(32),
   firstname     VARCHAR(32),
   lastname      VARCHAR(32),
   email         VARCHAR(100),
   phone         VARCHAR(64),
-  FOREIGN KEY (account_id) REFERENCES accounts(id)
+  FOREIGN KEY (username) REFERENCES accounts(username)
 );
 
 CREATE TABLE employers (
   id            INT AUTO_INCREMENT PRIMARY KEY,
-  account_id    INT,
+  username 	VARCHAR(32),
   company       VARCHAR(64),
   contact       VARCHAR(64),
   email         VARCHAR(100),
   phone         VARCHAR(64),
-  FOREIGN KEY (account_id) REFERENCES accounts(id)
+  FOREIGN KEY (username) REFERENCES accounts(username)
 );
 
 CREATE TABLE categories (
