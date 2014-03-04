ce08cd6c0f1dcfe39fa1a63f729440806c8f19ca
Benatti79 <andreasbenatti@live.se>
2014-03-04 11:52:23 +0100

    SeperateController
    La till controller map, varje controller är sin egen fil istället för
    alla i en och samma.

---
+++ js/controller/profileCtrl.js
@@ -0,0 +1,8 @@
+  'use strict';
+
+var computenzControllers = angular.module('computenzControllers', []);
+
+ computenzControllers.controller('ProfileCtrl', ['$scope', '$routeParams',
+    function($scope,$http) {
+
+    }]);
---
+++ js/controller/registrationCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var computenzControllers = angular.module('computenzControllers', []);
+
+  computenzControllers.controller('RegCtrl', ['$scope','$http',
+    function($scope,$http) {
+
+    }]);
