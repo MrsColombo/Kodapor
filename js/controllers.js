bf02c4fdc0a2058fdcdd257f718d0983b5b844da
samsonoris <samson.piano@gmail.com>
2014-03-04 12:11:48 +0100

    Modified LoginCtrl and added a service to that controller

--- js/controllers.js
+++ js/controllers.js
@@ -7,13 +7,46 @@ var computenzControllers = angular.module('computenzControllers', []);
 
     }]);
 
-  computenzControllers.controller('LoginCtrl', ['$scope','$http',
-    function($scope,$http) {
-      $http.get('/php/main.php/loginHandlerAction=login&username=' + $scope.username + '&password=' + $scope.password)
-        .success(function(data){
-
+  computenzControllers.controller('LoginCtrl', ['$scope','$http','$location', 'UserService',
+    function($scope,$http,$location,UserService) {
+      $scope.sendForm = function(){
+        $http.get('/php/main.php/loginHandlerAction=login&username=' + $scope.user.username + '&password=' + $scope.user.password).success(function(data){
+          if(data.authenticated === true && data.username === $scope.user.username){
+            UserService.setUsername(data.username);
+            //$location.path('/home');
+            $scope.message = 'Your login succeeded';
+          }else{
+            $scope.message = 'Your login failed';
+          }
         });
-    }]);
+      };
+    }]).service('UserService', function(){
+      var user = {
+        username: null,
+        firstname: null,
+        lastname: null
+      };
+
+      return {
+        getUsername: function(){
+          console.log('userService get', user.username);
+          return user.username;
+        },
+        setUsername: function(n){
+          user.username = n;
+          console.log('userService set', n, user.username);
+        },
+
+        call: function(prop,val){
+          if(val){
+            user[prop] = val;
+          }
+          return user[prop];
+        }
+      };
+
+    });
+
 
 
   computenzControllers.controller('ProfileCtrl', ['$scope', '$routeParams',
